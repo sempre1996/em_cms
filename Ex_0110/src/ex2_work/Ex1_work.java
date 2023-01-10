b4b2c58e8dbe3f6fc2cf4687ee5cf29e84b6e996
ITSC <ITSC@DESKTOP-SBHN298>
2023-01-10 15:38:06 +0900

    0110_project

--- Ex_0110/src/ex2_work/Ex1_work.java
+++ Ex_0110/src/ex2_work/Ex1_work.java
@@ -1,5 +1,7 @@
 package ex2_work;
 
+import java.util.Scanner;
+
 public class Ex1_work {
 	public static void main(String[] args) {
 		
@@ -8,6 +10,20 @@ public class Ex1_work {
 		// 입력 : 5
 		// 1 1 2 3 5 8 13 21
   		
+		Scanner sc = new Scanner(System.in);
+		int n1 = 1;
+		int n2 = 0;
+		System.out.print("입력 : ");
+		
+		int ss = sc.nextInt();
+		
+		for (int i = 0; i < ss; i++) {
+			int n3 = n1 + n2;
+			System.out.print(n3 + " ");
+			n1 = n2;
+			n2 = n3;
+		}
+		
 	}// main
 
 }
--- Ex_0110/src/ex2_work/Ex2_work.java
+++ Ex_0110/src/ex2_work/Ex2_work.java
@@ -1,5 +1,7 @@
 package ex2_work;
 
+import java.util.Scanner;
+
 public class Ex2_work {
 	public static void main(String[] args) {
 		
@@ -10,12 +12,47 @@ public class Ex2_work {
 	//08 01 06
 	//03 05 07
 	//04 09 02
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	
+	int[][] m = new int[x][x];
+	
+	int i = 0;
+	int j = x / 2;
+	
+	for (int a = 0; a <= x * x; a++) {
+		m[i][j] = a;
+		if ( a % x != 0) {
+			if (i == 0) {
+				i = x - 1;
+			}else {
+				i--;
+			}// else 
+			if (j == x - 1) 
+				j = 0;
+			else 
+				j++;
+		 }  else { if (i == x - 1) {
+				   i = 0;
+			}else 
+				i++;
+			}
+				
+			
+		}// out if
+		for (int k = 0; k < m.length; k++) {
+			for (int k2 = 0; k2 < m[k].length; k2++) {
+				 System.out.printf("%02d ",m[k][k2]);
+			}
+			System.out.println();
+		}
 		
 		
-		
+	}//for
+	
 	}// main
 
-}
+
 
 
 
